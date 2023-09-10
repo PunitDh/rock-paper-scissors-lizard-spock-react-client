8f497e20ac891403bbd8cc952712b04358f39957
Punit Dharmadhikari <punit.dharmadhikari@insigniafinancial.com.au>
2023-09-10 16:13:41 +1000

    Fix styling

--- src/views/Apps/components/LinkCard.jsx
+++ src/views/Apps/components/LinkCard.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FlexBox } from "src/components/shared/styles";
 
@@ -7,8 +7,9 @@ const CardLink = styled(Link)(({ theme }) => ({
   display: "flex",
   gap: "1rem",
   transform: "scale(1.0)",
-  transition: "transform 250ms ease-in",
+  transition: "transform 150ms ease-in",
   border: "1px solid rgba(0,0,0,0.1)",
+  backgroundColor: "rgba(210,210,255,0.1)",
   boxShadow: "10px 10px 10px -10px rgba(0,0,0,0.8)",
   borderRadius: "0.5rem",
   flexDirection: "column",
@@ -38,23 +39,25 @@ const TextContainer = styled.div({
   width: "100%",
   height: "30%",
   justifySelf: "flex-end",
-  gap: "0.75rem"
+  gap: "0.75rem",
 });
 
 const LinkCard = ({ to, Icon, title, description }) => (
-  <CardLink to={to}>
-    <ImageContainer>
-      <Icon fill="white" width="50%" />
-    </ImageContainer>
-    <TextContainer>
-      <FlexBox alignItems="flex-start" textAlign="center" height="3rem">
-        <Typography variant="h6">{title}</Typography>
-      </FlexBox>
-      <Typography variant="body2" color="text.secondary" textAlign="center">
-        {description}
-      </Typography>
-    </TextContainer>
-  </CardLink>
+  <Tooltip title={title} disableInteractive>
+    <CardLink to={to}>
+      <ImageContainer>
+        <Icon fill="none" width="50%" />
+      </ImageContainer>
+      <TextContainer>
+        <FlexBox alignItems="flex-start" textAlign="center" height="2.5rem">
+          <Typography variant="h6">{title}</Typography>
+        </FlexBox>
+        <Typography variant="body2" color="text.secondary" textAlign="center">
+          {description}
+        </Typography>
+      </TextContainer>
+    </CardLink>
+  </Tooltip>
 );
 
 export default LinkCard;
