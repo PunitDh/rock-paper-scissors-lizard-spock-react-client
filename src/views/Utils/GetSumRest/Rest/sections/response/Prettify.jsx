ba2d54975335ab88b5d5bd1d4a522ab4c6ced3fa
Punit Dharmadhikari <punit.dharmadhikari@insigniafinancial.com.au>
2023-09-11 22:38:57 +1000

    Prettify working beautifully

--- src/views/Utils/GetSumRest/Rest/sections/response/Prettify.jsx
+++ src/views/Utils/GetSumRest/Rest/sections/response/Prettify.jsx
@@ -13,16 +13,12 @@ const Green = styled.span({
   color: "green",
 });
 
-const Yellow = styled.span({
-  color: "yellow",
-});
-
-const Orange = styled.span({
-  color: "orange",
-});
-
 const Purple = styled.span({
   color: "purple",
+});
+
+const Black = styled.span({
+  color: "black",
 });
 
 const Margin = styled.span(({ depth }) => ({
@@ -30,51 +26,76 @@ const Margin = styled.span(({ depth }) => ({
 }));
 
 const Prettify = ({ children }) => {
-  const keys = Object.keys(children);
-  const values = Object.values(children);
-
-  console.log({ children });
+  if (!children) return <span>null</span>;
 
   const prettify = (object, depth = 1) => {
-    const keys = Object.keys(object);
-    return (
-      <>
-        {"{"}
-        <br />
-        {keys.map((key, index) => (
-          <Margin key={key} depth={depth}>
-            <Red>"</Red>
-            <Red>{key}</Red>
-            <Red>"</Red>:{" "}
-            {typeof object[key] === "number" ? (
-              <Green>{object[key]}</Green>
-            ) : typeof object[key] === "boolean" ? (
-              <Purple>{object[key].toString()}</Purple>
-            ) : typeof object[key] === "string" ? (
-              <>
+    if (Array.isArray(object)) {
+      return (
+        <>
+          <Black>[</Black>
+          <br />
+          {object.map((item, index) => (
+            <Margin key={index} depth={depth}>
+              {typeof item === "number" ? (
+                <Green>{item}</Green>
+              ) : typeof item === "boolean" ? (
+                <Purple>{item.toString()}</Purple>
+              ) : typeof item === "string" ? (
                 <Blue>
-                  <Blue>"</Blue>
-                  {object[key]}
-                  <Blue>"</Blue>
+                  <Red>"</Red>
+                  {item}
+                  <Red>"</Red>
                 </Blue>
-              </>
-            ) : Array.isArray(object[key]) ||
-              (object[key] && typeof object[key] === "object") ? (
-              prettify(object[key], depth + 1)
-            ) : (
-              JSON.stringify(object[key])
-            )}
-            {index !== keys.length - 1 && (
-              <>
-                ,<br />
-              </>
-            )}
+              ) : typeof item === "object" ? (
+                prettify(item, depth + 1)
+              ) : (
+                JSON.stringify(item)
+              )}
+              {index !== object.length - 1 && (
+                <>
+                  ,<br />
+                </>
+              )}
+            </Margin>
+          ))}
+          <br />
+          <Black>]</Black>
+        </>
+      );
+    } else {
+      const keys = Object.keys(object);
+      return (
+        <>
+          <Black>{"{"}</Black>
+          <br />
+          {keys.map((key, index) => (
+            <Margin key={key} depth={depth}>
+              <Red>"{key}"</Red>:{" "}
+              {typeof object[key] === "number" ? (
+                <Green>{object[key]}</Green> // Numbers are printed Green
+              ) : typeof object[key] === "boolean" ? (
+                <Purple>{object[key].toString()}</Purple> // Booleans are purple
+              ) : typeof object[key] === "string" ? (
+                <Blue>"{object[key]}"</Blue> // Strings are blue
+              ) : typeof object[key] === "object" ? (
+                prettify(object[key], depth + 1)
+              ) : (
+                JSON.stringify(object[key])
+              )}
+              {index !== keys.length - 1 && (
+                <>
+                  ,<br />
+                </>
+              )}
+            </Margin>
+          ))}
+          <br />
+          <Margin depth={depth - 1}>
+            <Black>{"}"}</Black>
           </Margin>
-        ))}
-        <br />
-        {"}"}
-      </>
-    );
+        </>
+      );
+    }
   };
 
   return <div>{prettify(children)}</div>;
